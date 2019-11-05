ab7f61052a473b5de14c7c640be77c7a9b01b7ad
Salv8 <salv888@gmail.com>
2019-11-05 17:25:14 -0300

    primer commit

---
+++ primera-prueba/js/alumnos.js
@@ -0,0 +1,8 @@
+function AlumnosController($scope) {
+	$scope.alumnos = [
+		{nombre:"Arturo Vidal" , telefono:"9856257", equipo:"Barcelona"},
+		{nombre:"Alexis Sanchez" , telefono:"874521", equipo:"Inter de Milan"},
+		{nombre:"Claudio Bravo" , telefono:"235687", equipo:"Manchester City"},
+		{nombre:"Felipe Flores" , telefono:"2874569", equipo:"Antofagasta"}
+	]
+}
