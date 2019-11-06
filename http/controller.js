5c5024d907e209a1306fc967ecdf1aafb49afac2
Salv8 <salv888@gmail.com>
2019-11-06 19:16:56 -0300

    Agregada segunda parte parte $http ajax post

--- http/controller.js
+++ http/controller.js
@@ -1,7 +1,12 @@
+/**
+ * Prueba completa de ajax utilizando get y post
+ * https://codigofacilito.com/videos/curso_angularjs_ajax_con_http_1
+ */
 
 angular.module("app", [] )
 	.controller('ControllerHTTP', ['$scope','$http', function(e,h){
 		e.posts = [];
+		e.newPost = {};
 		h.get('https://jsonplaceholder.typicode.com/posts')
 			.then(function(response){
 				e.posts = response.data;
@@ -9,4 +14,19 @@ angular.module("app", [] )
 			,function(err){
 
 			};
+		e.addPost = function(){
+			h.post('https://jsonplaceholder.typicode.com/posts', {
+				title : e.newPost.title,
+				body : e.newPost.body,
+				userId : 1
+			} )
+				.then(function(response){
+					e.posts.push(response.data);
+					e.newPost = {};
+
+				})
+				,function(error){
+					console.log(error);
+				};
+		}	
 	}]);
